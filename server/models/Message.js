2446efb451059dde2eef2040021565a4af96548a
[Bini Chand] <[079bct0b33.bini@pcampus.edu.np]>
2024-11-15 21:24:39 +0545

    chat thread configured

---
+++ server/models/Message.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+    
+    userId: { type: String, required: true },
+    username: { type: String, required: true },
+    content: { type: String, required: true },
+    timestamp: { type: Date, default: Date.now },
+    
+});
+
+const Message = mongoose.model("Message", messageSchema);
+export default Message;
